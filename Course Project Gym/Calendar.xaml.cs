b1ddf826574a445149285f49f58c33f8b8bb621c
yarik.havrylov <yarik.havrylov@gmail.com>
2018-12-14 20:34:32 +0200

    update

--- Course Project Gym/Calendar.xaml.cs
+++ Course Project Gym/Calendar.xaml.cs
@@ -24,5 +24,10 @@ namespace Course_Project_Gym
         {
             InitializeComponent();
         }
+
+        private void StackPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
     }
 }
