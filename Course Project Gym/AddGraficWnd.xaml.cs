1cf07912244e5c3ca04219d6294f574cd2cd5e77
Oleg <oleg.mel123@gmail.com>
2018-12-20 16:57:41 +0200

    update

--- Course Project Gym/AddGraficWnd.xaml.cs
+++ Course Project Gym/AddGraficWnd.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Course_Project_Gym.DataBase;
+using Course_Project_Gym.DataBase.Repositories;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,16 +21,58 @@ namespace Course_Project_Gym
     /// <summary>
     /// Interaction logic for AddGraficWnd.xaml
     /// </summary>
-    public partial class AddGraficWnd : Window
+    public partial class AddGraficWnd : Window, INotifyPropertyChanged
     {
+        public List<Staff> Coach { get; set; }
+        private List<Schedules> schedules = new List<Schedules>();
+        public List<Schedules> Schedules
+        {
+            get
+            {
+                return schedules;
+            }
+            set
+            {
+                schedules = value;
+                OnPropertyChanged("Schedules");
+            }
+        }
+
         public AddGraficWnd()
         {
+            DataContext = this;
+
+            foreach (var item in StaffRepository.GetInstance().GetAll())
+            {
+                if (item.Position.Name.Equals("Coach"))
+                {
+                    Coach.Add(item);
+                }
+            }
             InitializeComponent();
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
         }
 
         private void AddFinallyBtn_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void NewsNameCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (var item in SchedulesRepository.GetInstance().GetAll())
+            {
+                if (item.Coach.Id == ((sender as ComboBox).SelectedItem as Staff).Id)
+                {
+                    Schedules.Add(item);
+                }
+            }
         }
     }
 }
--- Course Project Gym/DataBase/Entity Models/Staff.cs
+++ Course Project Gym/DataBase/Entity Models/Staff.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Course_Project_Gym.DataBase
+﻿namespace Course_Project_Gym.DataBase
 {
     public class Staff
     {
         public int Id { get; set; }
-
+        
         public string Name { get; set; }
         public string SurName { get; set; }
         public string PhoneNumber { get; set; }
